d0bfce2c2cd3bbe737a155d99401d2896343773c
Sebastian Kessler <sebastian@herrkessler.de>
2015-01-09 15:26:55 +0100

    Add new Post routes

--- main.rb
+++ main.rb
@@ -228,11 +228,22 @@ class KletterPartner < Sinatra::Base
     @posts = Post.all
     slim :"post/index"
   end
+  
+  get '/blog/new' do
+    @post = Post.new
+    slim :"post/new"
+  end
 
+  post '/blog' do
+    @post = Post.create(params[:post])
+    flash[:success] = 'Neuer Post: "' + @post.title + '" angelegt'
+    redirect to("/blog/#{@post.id}")
+  end
 
   get '/blog/:id' do
     @post = Post.get(params[:id])
     slim :"post/show"
   end
+
 
 end
