3334f53c2a9fba8c3e63222f66b94fe91912fdc6
Sebastian Kessler <sebastian@herrkessler.de>
2015-01-16 16:18:42 +0100

    Messaging #new and #reply

--- assets/js/specific/message.js
+++ assets/js/specific/message.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
+
+  // Settings
+  // -------------------------------
   var message = $('#messages'),
       firstConversation = $('#conversations-list li').first(),
       conversationId = $('#conversations-list li').first().data('id'),
       userId = $('#conversations').data('id'),
       messagesList = $('.messages-list'),
-      messageSpinner = $('#message-spinner');
-      conversationLink = $('.conversations-list-item');
+      messageSpinner = $('#message-spinner'),
+      conversationLink = $('.conversations-list-item'),
+      answerText = $('#message-answer'),
+      answerLink = $('#message-answer-link'),
+      view = window.location.href;
+
+  // Get Messages
+  // -------------------------------
 
   firstConversation.addClass('selected');
+  message.attr('data-id', conversationId);
 
-  getMessages(userId, conversationId);
+  if(view.indexOf("messages") > -1) {
+    getMessages(userId, conversationId);
+  }
   
   $(document).ajaxStart(function() {
     messageSpinner.addClass('loading');
   });
-
   $(document).ajaxComplete(function() {
     messageSpinner.removeClass('loading');
   });
@@ -23,20 +34,82 @@ $(document).ready(function(){
     var messageId = $(this).data('id');
     $(this).addClass('selected');
     $(this).siblings('.conversations-list-item').removeClass('selected');
+    message.attr('data-id', messageId);
     getMessages(userId, messageId);
   });
 
-  function getMessages(user, message) {
-    $.getJSON( "/users/"+user+"/messages/"+message, {
+  answerLink.on('click', function(event){
+    event.preventDefault();
+    if (answerText.val().length === 0) {
+      answerText.focus();
+    } else {
+      var messageText = answerText.val();
+      var selectedConversation = $('#messages').attr('data-id');
+
+      $.ajax({
+        url: "/users/"+userId+"/messages/"+selectedConversation,
+        dataType: 'json',
+        contentType: 'application/json',
+        type: 'POST',
+        data : messageText,
+        accepts: "application/json",
+        success: function() {
+          getMessages(userId, selectedConversation);
+          answerText.val('');
+        }, 
+        error: function(json) {
+          console.log(json);
+        }
+      });
+    }
+  });
+
+  // New Message
+  // -------------------------------
+
+  var newMessageLink = $('#new-message');
+
+  newMessageLink.on('click', function(event){
+    event.preventDefault();
+    var messageText = 'Static Text';
+    var newConversation = '1';
+    var recieverId = '4';
+
+    $.ajax({
+      url: "/users/"+userId+"/new-message/"+recieverId,
+      dataType: 'json',
+      contentType: 'application/json',
+      type: 'POST',
+      data : messageText,
+      accepts: "application/json",
+      success: function() {
+        getMessages(userId, selectedConversation);
+        console.log(json);
+      }, 
+      error: function(json) {
+        console.log(json);
+      }
+    });
+  });
+
+  // Functions
+  // -------------------------------
+
+  function scrollMessageBottom() {
+    $("#messages").scrollTop($(".messages-list-item:last-child").position().top);
+  }
+
+  function getMessages(userID, messageID) {
+    $.getJSON( "/users/"+userID+"/messages/"+messageID, {
     }).done(function( data ) {
       $('.messages-list-item').remove();
       $.each(data, function(index, message){
         messagesList.append(
           '<li class="messages-list-item message_'+index+'"><div class="message-header"><div class="message-title">'+message.title+'</div><div class="message-date message-timestamp">'+message.created_at+'</div></div><div class="message-content">'+message.content+'</div></li>'
-          );
+        );
+        scrollMessageBottom();
       });
     });
   }
-
 
 });
--- routes/messages.rb
+++ routes/messages.rb
@@ -7,7 +7,7 @@ class KletterPartner < Sinatra::Base
     env['warden'].authenticate!
     @sessionUser = env['warden'].user
     @user = User.get(params[:id])
-    @my_conversations = @user.conversations.sort! {  |a, b|  a.update_at <=> b.update_at }
+    @my_conversations = @user.conversations.sort! {  |a, b|  a.update_at <=> b.update_at }.reverse
     @email_stats = @my_conversations.messages.map(&:status)
     @newest_conversation = @my_conversations.first
     slim :"message/index"
@@ -18,6 +18,51 @@ class KletterPartner < Sinatra::Base
     conversation = Conversation.get(params[:conversation])
     messages = conversation.messages
     messages.to_json
+  end
+
+  post '/users/:id/messages/:conversation', :provides => :json do
+    data = request.body.read
+    @conversation = Conversation.get(params[:conversation])
+    @message = Message.new()
+    @message.attributes = {:conversation => @conversation, :title => 'Static Title', :content => data}
+    @message.save
+    @conversation.messages << @message
+    @conversation.save
+    @conversation.messages.save
+    @conversation.update(:update_at => Time.now)
+    halt 200, data.to_json
+  end
+
+  post '/users/:id/new-message/:reciever', :provides => :json do
+    data = request.body.read
+
+    conversation = Conversation.new()
+    conversation.save
+
+    message = Message.new()
+    message.attributes = {:conversation => conversation, :title => 'Static Title', :content => data}
+    message.save
+
+    sender = Participant.new()
+    senderUser = User.get(params[:id])
+    sender.attributes = {:conversation => conversation, :user => senderUser}
+    sender.save
+
+    reciever = Participant.new()
+    recieverUser = User.get(params[:reciever])
+    reciever.attributes = {:conversation => conversation, :user => recieverUser}
+    reciever.save
+
+    conversation.messages << message
+    conversation.participants << sender
+    conversation.participants << reciever
+    conversation.save
+    conversation.messages.save
+    conversation.participants.save
+
+    conversation.update(:update_at => Time.now)
+
+    halt 200, data.to_json
   end
 
 end
