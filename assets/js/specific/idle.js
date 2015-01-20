a8c7100ab696e7b14c55b538d7c7f837399c07b5
Sebastian Kessler <sebastian@herrkessler.de>
2015-01-20 08:45:29 +0100

    delete initial status call

--- assets/js/specific/idle.js
+++ assets/js/specific/idle.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
   if ($('body').hasClass('logged-in')) {
     $(function() {
+
+      var status = 'online';
       
       $( document ).idleTimer( {
         timeout: 300000, 
@@ -9,6 +11,9 @@ $(document).ready(function() {
       });
 
       $( document ).on( "idle.idleTimer", function(event, elem, obj){
+
+        status = 'idle';
+
         $.ajax({
           url: "/user/idle",
           dataType: 'json',
@@ -19,13 +24,18 @@ $(document).ready(function() {
       });
 
       $( document ).on( "active.idleTimer", function(event, elem, obj, triggerevent){
-        $.ajax({
-          url: "/user/online",
-          dataType: 'json',
-          contentType: 'application/json',
-          type: 'GET',
-          accepts: "application/json"
-        });
+
+        if (status != 'online') {
+          status = 'online';
+
+          $.ajax({
+            url: "/user/online",
+            dataType: 'json',
+            contentType: 'application/json',
+            type: 'GET',
+            accepts: "application/json"
+          });
+        }
       });
     });
   }
