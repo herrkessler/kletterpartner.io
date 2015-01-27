e6ad1277cb3d903f63c3de7bbd45255f1e5e5ddb
Sebastian Kessler <sebastian@herrkessler.de>
2015-01-27 16:45:02 +0100

    login fronted stuff

--- routes/index.rb
+++ routes/index.rb
@@ -4,31 +4,9 @@ class KletterPartner < Sinatra::Base
   # -----------------------------------------------------------
 
   get '/' do
-    if !request.websocket?
-      unless env['warden'].user == nil
-        @email_stats ||= env['warden'].user.conversations.messages.map(&:status) || halt(404)
-      end
-
-      slim :"index/index"
-    else
-      request.websocket do |ws|
-        ws.onopen do
-          ws.send("Hello World!")
-          settings.sockets << ws
-        end
-        ws.onmessage do |msg|
-          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-        end
-        ws.onclose do
-          warn("websocket closed")
-          settings.sockets.delete(ws)
-        end
-      end
-
+    unless env['warden'].user == nil
+      @email_stats ||= env['warden'].user.conversations.messages.map(&:status) || halt(404)
     end
-  end
-
-  post '/messages' do
-    Pusher['test_channel'].trigger('new_message', :message => params['message'])
+    slim :"index/index"
   end
 end
